4c10b2d8612007b3c99dabb87952e69b7fc12086
FlaringK <caelans@iinet.net.au>
2023-06-19 22:15:17 +0800

    Test 2

--- index.js
+++ index.js
@@ -55,9 +55,10 @@ async function genUTimage (req, res) {
 	// Load assets
 	let faces = await loadImage(path.join(__dirname, `public`, `chara.png`)).catch(() => "404");
 	let textbox = await loadImage(path.join(__dirname, `public`, `tb_basic.png`)).catch(() => "404");
-	const dtFont = new FontFace("dt", 'url("https://file.garden/X1htvgJ0DEp_tp-Z/MSPFA%20undertale/Fonts/DeterminationMonoWebRegular-Z5oq.ttf")')
-  await dtFont.load()
-  document.fonts.add(dtFont);
+
+	// const dtFont = new FontFace("dt", 'url("https://file.garden/X1htvgJ0DEp_tp-Z/MSPFA%20undertale/Fonts/DeterminationMonoWebRegular-Z5oq.ttf")')
+  // await dtFont.load()
+  // document.fonts.add(dtFont);
 	
 	ctx.textBaseline = 'alphabetic'
   ctx.font = "32px dt"
