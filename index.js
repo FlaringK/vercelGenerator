ccd27f3ccf2da194c8b743f303a58bb0a0a75580
FlaringK <caelans@iinet.net.au>
2023-06-22 19:29:37 +0800

    Refactor

--- index.js
+++ index.js
@@ -168,39 +168,38 @@ let processUTtext = inText => {
 
 async function genHSimage (req, res) {
 
-  const spriteSizes = {
-    ob: { x: 175, y: 240, ox: -80, oy: -15 },
-    bdth: { x: 260, y: 325, ox: -80, oy: 0 }
+  // Set img type
+  res.set('Cache-Control', "public, max-age=300, s-maxage=600");
+  res.set('Content-Type', 'image/png');
+
+  // Consts
+  const dimentions = {
+    default: {
+      x: 175, y: 240, ox: -80, oy: -15, scale: 2, width: 650, height: 450,
+      pos: { text: 84, width: 475, box: 56, height: 110, }, lineHeight: 16,
+    },
+    ob: { 
+      x: 175, y: 240, ox: -80, oy: -15, scale: 2, width: 650, height: 450,
+      pos: { text: 250, width: 360, box: 90, height: 110, }, lineHeight: 16,
+    },
+    bdth: { 
+      x: 260, y: 325, ox: 480, oy: 0, scale: 2, width: 930, height: 650,
+      pos: { text: 48, width: 400, box: 18, height: 180, }, lineHeight: 20,
+    }
   }
 
   const characters = {
-    karkat: { col: "#626262", sprite: "ob_karkat.png", lean: 2, dim: spriteSizes.ob },
-    rose: { col: "#b536da", sprite: "ob_rose.png", lean: 2, dim: spriteSizes.ob },
-    bdthJune: { col: "#b536da", sprite: "bdth_june.png", lean: 1, dim: spriteSizes.bdth },
+    karkat: { col: "#626262", sprite: "ob_karkat.png", lean: 2, dim: dimentions.ob },
+    rose: { col: "#b536da", sprite: "ob_rose.png", lean: 2, dim: dimentions.ob },
+    bdthJune: { col: "#0715cd", sprite: "bdth_june.png", lean: 1, dim: dimentions.bdth },
   }
 
   const text = req.params.text ?? "Wow%2C%20you%20must%27ve%20really%20fucked%20something%20up"
   const char = characters[req.params.char] ?? characters.karkat
 
-  // Set img type
-  res.set('Cache-Control', "public, max-age=300, s-maxage=600");
-  res.set('Content-Type', 'image/png');
-
   // Create canvas
-  const canvas = createCanvas(650, 450);
+  const canvas = createCanvas(char.dim.width, char.dim.height);
   const ctx = canvas.getContext('2d');
-
-  const positions = [
-    { // Center
-      text: 84, width: 475, box: 56
-    },
-    { // left
-      text: 48, width: 360, box: 18
-    },
-    { // right
-      text: 250, width: 360, box: 90
-    },
-  ]
 
   // get images
   let bigbox = await loadImage(path.join(__dirname, `public`, `homestuck`, `dialogBoxBig.png`)).catch(() => "404");
@@ -222,16 +221,17 @@ async function genHSimage (req, res) {
     face = face ? face : 0
   }
 
-  const pos = positions[lean]
+  const pos = char.dim.pos
 
+  // BEGIN DRAWING
   // Draw Textbox
-  ctx.drawImage(bigbox, pos.box, 110)
-  ctx.drawImage(smallbox, pos.box, 380)
+  ctx.drawImage(bigbox, pos.box, pos.height)
+  ctx.drawImage(smallbox, pos.box, pos.height + 270)
 
   // Get text colour
   let textColor = lean ? char.col : "black"
 
-  // Set text params
+  // Draw text
   ctx.font = "16px CourierNewBold"
   ctx.imageSmoothingEnabled = false;
   ctx.fillStyle = textColor
@@ -241,7 +241,7 @@ async function genHSimage (req, res) {
 
   let bodyLines = splitLines(ctx, textTypes[0].replace(startReg, ""), pos.width)
   bodyLines.forEach((line, i) => {
-    ctx.fillText(line, pos.text, 150 + 16 * i)
+    ctx.fillText(line, pos.text, pos.height + 40 + char.dim.lineHeight * i)
   })
 
   // Draw Hashtag text
@@ -250,7 +250,7 @@ async function genHSimage (req, res) {
   if (textTypes[1]) {
     let hashLines = splitLines(ctx, "#" + textTypes[1], pos.width)
     hashLines.forEach((line, i) => {
-      ctx.fillText(line, pos.text, (hashLines.length > 2 ? 400 : 408) + 16 * i)
+      ctx.fillText(line, pos.text, (hashLines.length > 2 ? pos.height + 290 : pos.height + 298) + 16 * i)
     })
   }
 
@@ -258,7 +258,7 @@ async function genHSimage (req, res) {
   let sprite = char.dim
 
   if (lean) {
-    ctx.drawImage(faces, 0, sprite.y * face, sprite.x, sprite.y, sprite.ox, sprite.oy, sprite.x * 2, sprite.y * 2)
+    ctx.drawImage(faces, 0, sprite.y * face, sprite.x, sprite.y, sprite.ox, sprite.oy, sprite.x * sprite.scale, sprite.y * sprite.scale)
   }
 
   // Send Canvas
