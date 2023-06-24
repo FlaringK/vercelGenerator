f2e0ec2e7792b14c35bc71390ccaadc4f42c5e2c
FlaringK <caelans@iinet.net.au>
2023-06-24 21:31:45 +0800

    cached???

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ async function genHSimage (req, res) {
 
     bdthJune: { col: "#0715cd", sprite: "bdth_june.png", dim: dimentions.bdth, handle: "JUNE" },
     bdthRose: { col: "#b536da", sprite: "bdth_rose.png", dim: dimentions.bdth, handle: "ROSE" },
-    bdthDave: { col: "#e00707", sprite: "bdth_dave.png", dim: dimentions.bdthSS, handle: "DAVE" },
+    bdthDave: { col: "#e00707", sprite: "bdth_dave_.png", dim: dimentions.bdthSS, handle: "DAVE" },
     bdthJade: { col: "#4ac925", sprite: "bdth_jade.png", dim: dimentions.bdth, handle: "JADE" },
     bdthKanaya: { col: "#008141", sprite: "bdth_kanaya.png", dim: dimentions.bdth, handle: "KANAYA" },
   }
